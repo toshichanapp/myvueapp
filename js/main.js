62e1f2f5e802edd4ef16d7a3daaf8d9e0ad4335d
toshichanapp <toshichan@synapz.jp>
2018-09-09 21:25:07 +0900

    起動時にlocalStorageからデータを取り出す

--- js/main.js
+++ js/main.js
@@ -5,29 +5,18 @@
         el: '#app',
         data: {
             newItem: '',
-            todos: [{
-                title: 'task 1',
-                isDone: false
-            }, {
-                title: 'task 2',
-                isDone: false
-            }, {
-                title: 'task 3',
-                isDone: true
-            }]
+            todos: []
         },
         watch: {
-            // todos: function() {
-            //   localStorage.setItem('todos', JSON.stringify(this.todos));
-            //   alert('Data saved!');
-            // }
             todos: {
                 handler: function() {
                     localStorage.setItem('todos', JSON.stringify(this.todos));
-                    // alert('Data saved!');
                 },
                 deep: true
             }
+        },
+        mounted: function() {
+            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
         },
         methods: {
             addItem: function() {
